d6a0b7abd2d8133af36beecf39b1daa907685f41
kpatoukpakpodjro <kpatoukpakpodjro@gmail.com>
2022-11-30 08:22:24 +0100

    gestion de site e commerce

---
+++ src/main/java/lsi/fstt/bean/CategorieManagedBean.java
@@ -0,0 +1,124 @@
+package lsi.fstt.bean;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+ import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.Part;
+
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+import lsi.fstt.persistence.Categorie;
+import lsi.fstt.persistence.DatabaseOperations;
+
+@ApplicationScoped
+@ManagedBean(name = "CategorieManagedBean")
+@MultipartConfig
+public class CategorieManagedBean {
+	private int idCat;
+	private String libelle;
+	private Part img;
+	private StreamedContent image;
+	
+	public int getIdCat() {
+		return idCat;
+	}
+
+	public CategorieManagedBean() {
+		super();
+	}
+	public void setIdCat(int idCat) {
+		this.idCat = idCat;
+	}
+
+	public Part getImg() {
+		return img;
+	}
+
+	public void setImg(Part img) {
+		this.img = img;
+	}
+
+	public void setImage(StreamedContent image) {
+		this.image = image;
+	}
+
+	//ajouter une categorie
+	public String save(CategorieManagedBean bean) throws IOException {
+		InputStream input = img.getInputStream();
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		byte[] buffer = new byte[10240];
+		for (int length = 0; (length = input.read(buffer)) > 0;)
+			output.write(buffer, 0, length);
+		return DatabaseOperations.creerCategorie(bean.libelle , output.toByteArray());
+	}
+	
+	
+	public StreamedContent getImage() throws IOException {
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+			this.image = new DefaultStreamedContent();
+			return this.image;
+		} else {
+			String imageId = context.getExternalContext().getRequestParameterMap().get("idCat");
+			byte[] myImage = null;
+			for (Categorie cat : listCategories()) {
+				if (cat.getIdCat() == Integer.valueOf(imageId)) {
+					myImage = cat.getImgCat();
+					break;
+				}
+			}
+			this.image = new DefaultStreamedContent(new ByteArrayInputStream(myImage));
+			return this.image;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Categorie> listCategories() {
+		return DatabaseOperations.getAllCategories();
+	}
+
+	public String redirectUpdate(int idCat) {
+		return "updateCategorie.xhtml?faces-redirect=true&idCat="+idCat;
+	}
+	
+	public String redirectProduit(int idCat)
+	{
+		return "listProduitsByCat.xhtml?faces-redirect=true&idCat="+idCat;
+	}
+	
+	public String redirectDelete(int idCat)
+	{
+		return DatabaseOperations.deleteCategorie(idCat);
+	}
+	
+	public int setCategorie(int id)
+	{
+		this.idCat = DatabaseOperations.getCategorieById(id).getIdCat();
+		this.libelle = DatabaseOperations.getCategorieById(id).getNomCat();
+		return this.idCat;
+	}
+
+	public String updateCategorie(CategorieManagedBean bean) throws IOException
+	{
+		InputStream input = img.getInputStream();
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		byte[] buffer = new byte[10240];
+		for (int length = 0; (length = input.read(buffer)) > 0;)
+			output.write(buffer, 0, length);
+		
+		Categorie cat = new Categorie(bean.idCat, bean.libelle, output.toByteArray());
+		return DatabaseOperations.updateCategorie(cat);
+	}
+	
+	
+	
+}
---
+++ src/main/java/lsi/fstt/persistence/Categorie.java
@@ -0,0 +1,102 @@
+package lsi.fstt.persistence;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorie")
+public class Categorie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, unique = true)
+	private int idCat;
+
+	@Column(nullable = true, length = 45)
+	private String libelle;
+	
+	@Column(nullable = true, length = 52428800)
+	@Lob
+	private byte[] imgCat;
+	
+	@OneToMany
+	@JoinColumn(name = "id_cat")
+	private List<Produit> listProd;
+
+	public Categorie() {
+		// TODO Auto-generated constructor stub
+		super();
+		
+	}
+
+	public Categorie(String libelle, byte[] img) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.libelle = libelle;
+		this.imgCat = img ;
+	}
+
+	public Categorie(int id, String libelle2, byte[] img) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.idCat = id ;
+		this.libelle = libelle2;
+		this.imgCat = img ;
+	}
+
+	public int getIdCat() {
+		return idCat;
+	}
+
+	public void setIdCat(int idCat) {
+		this.idCat = idCat;
+	}
+
+	public String getNomCat() {
+		return libelle;
+	}
+
+	public void setNomCat(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public List<Produit> getListProd() {
+		return listProd;
+	}
+
+	public void setListProd(List<Produit> listProd) {
+		this.listProd = listProd;
+	}
+	
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public byte[] getImgCat() {
+		return imgCat;
+	}
+
+	public void setImgCat(byte[] imgCat) {
+		this.imgCat = imgCat;
+	}
+
+	@Override
+	public String toString() {
+		return "Categorie [idCat=" + idCat + ", nomCat=" + libelle + "]";
+	}
+
+}
